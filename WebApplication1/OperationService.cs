604e70079260a9156839fc4576a5a641137e3768
Missaxe <79321107+Missaxe@users.noreply.github.com>
2025-03-16 15:14:01 +0100

    pushing project

---
+++ WebApplication1/OperationService.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1
+{
+    public class OperationService : ITransientService, ISingletonService ,IScopedService
+    {
+        private readonly Guid id;
+        public OperationService()
+        {
+            id = Guid.NewGuid();
+            
+        }
+        public Guid GetOperationID()
+        {
+            return id;
+        }
+    }
+}
